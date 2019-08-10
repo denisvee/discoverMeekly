103bd23849b134b50b1f33853c9c42cc35f55eb8
Denis <denisvashchenko@gmail.com>
2019-08-10 12:11:46 -0700

    Added Spotify auth flow.

--- DiscoverGrab.cs
+++ DiscoverGrab.cs
@@ -4,28 +4,47 @@ using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace Company.Function
 {
     public static class DiscoverGrab
     {
         [FunctionName("DiscoverGrab")]
-        public static void Run([TimerTrigger("0 0 23 * * MON")]TimerInfo myTimer, ILogger log)
+        public static async Task Run([TimerTrigger("0 0 23 * * MON")]TimerInfo myTimer, ILogger log)
         {
+            var accessToken = await AuthenticateSpotify();
+            await GetDiscoverWeekly(accessToken); //probably store in the queue and reference with another func
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         }
-
-        public static async Task<string> GetDiscoverWeekly()
+        public static async Task<string> AuthenticateSpotify()
+        {
+            HttpClient client = new HttpClient();
+            byte[] grantArray = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("SPOTIFY_AUTH"));
+            byte[] authArray = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("SPOTIFY_CLIENT_CREDENTIALS"));
+            var authString = System.Convert.ToBase64String(authArray);
+            var grantBody = System.Web.HttpUtility.UrlEncode(grantArray);
+            client.DefaultRequestHeaders.Add("Authorizaton", authString);
+            using (HttpResponseMessage response = await client.PostAsJsonAsync(Environment.GetEnvironmentVariable("SPOTIFY_AUTH_ENDPOINT"), grantBody))
+            {
+                var accessToken = await response.Content.ReadAsStringAsync();
+                return accessToken;
+            }
+        }
+        public static async Task<object> GetDiscoverWeekly(string accessToken)
         {
             HttpClient client = new HttpClient(); 
-            
-            using (HttpResponseMessage response = await client.GetAsync(Environment.GetEnvironmentVariable("DISCOVER_TRACKS_ENDPOINT")))
+            client.DefaultRequestHeaders.Add("Authorization", accessToken);
+            using (HttpResponseMessage response = await client.GetAsync(Environment.GetEnvironmentVariable("SPOTIFY_DISCOVER_TRACKS_ENDPOINT")))
             {
                 if (response.StatusCode.Equals(200))
                 {
-                    return "Succes";
+                    Console.WriteLine("Successfuly got Spotify playlist data.");
+                    return JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync());
                 }
-                return "Failed";
+                Console.WriteLine("Failed to get Spotify playlist data.");
+                return new object();
             }
         }
     }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DiscoverMeekly
+
+## Leveraging Spotify's Discover Weekly recommender engine to understand trends in music taste over time. 
+
+## Utilizing Azure Funtions for serverless compute and Spotify API for song classification metadata. 
