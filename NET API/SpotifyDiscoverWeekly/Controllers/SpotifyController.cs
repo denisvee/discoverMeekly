e752cc2916ad4933cc400393b345f29a3e102116
Denis <denisvashchenko@gmail.com>
2019-08-25 17:11:49 -0700

    Successfully getting token via Client Credentials Auth

--- NET API/SpotifyDiscoverWeekly/Controllers/SpotifyController.cs
+++ NET API/SpotifyDiscoverWeekly/Controllers/SpotifyController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,18 +8,34 @@ using Newtonsoft.Json;
 
 namespace SpotifyDiscoverWeekly.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("/[controller]")]
     [ApiController]
     public class SpotifyController : ControllerBase
     {
         [HttpGet]
-        public async Task<string> GetTracks()
+        public async Task<IActionResult> GetDiscoverWeeklyTracks()
         {
-            var accessToken = Redirect("https://accounts.spotify.com/authorize?client_id={client_id}&response_type=code&redirect_uri={redirect_uri}");
-            //var discoverWeeklyTracks = await GetDiscoverWeekly(accessToken.);
-            return "succes";
+            var accessToken = await GetSpotifyToken();
+            await GetDiscoverWeekly(accessToken.AccessToken);
+            return Ok();
         }
+        public static async Task<Models.Token> GetSpotifyToken()
+        {
+            string auth = Convert.ToBase64String(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("SPOTIFY_CLIENT_ID") + ":" + Environment.GetEnvironmentVariable("SPOTIFY_CLIENT_SECRET")));
 
+            List<KeyValuePair<string, string>> args = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("grant_type", "client_credentials")
+            };
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", $"Basic {auth}");
+            HttpContent content = new FormUrlEncodedContent(args);
+
+            HttpResponseMessage resp = await client.PostAsync("https://accounts.spotify.com/api/token", content);
+            string msg = await resp.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<Models.Token>(msg);
+        }
         public static async Task<object> GetDiscoverWeekly(string accessToken)
         {
             HttpClient client = new HttpClient();
