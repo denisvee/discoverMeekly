83570eda87ae00d8a9f369d6e5961dfe94791991
Denis Vashchenko <denisvashchenko@gmail.com>
2020-01-19 16:33:23 -0800

    relevant feature analysis is returned (need to oopify)

--- dataPipe/__init__.py
+++ dataPipe/__init__.py
@@ -2,11 +2,66 @@ import logging
 import pyodbc
 import json
 import os
+import spotipy
+import spotipy.util as util
+import requests
+import numpy as np
 
 import azure.functions as func
 
-def average_analysis():
-    # Call spotify API and return average feature values for buckets
+def get_token():
+    refresh_token = os.environ['SPOTIFY_REFRESH_TOKEN']
+    client_id = os.environ['SPOTIFY_CLIENT_ID']
+    client_secret = os.environ['SPOTIFY_CLIENT_SECRET']
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    token_url = 'https://accounts.spotify.com/api/token'
+    token_params = {'refresh_token': refresh_token, 'client_id': client_id,'client_secret': client_secret, 'grant_type': 'refresh_token', 'redirect_uri': 'https://denisvashchenko.com/'}
+    response = requests.post(token_url, params=token_params, headers=headers)
+    return response.json()['access_token']
+
+def feature_analysis(bucket_uris):
+    final_buckets = []
+
+    scope = 'user-library-read'
+    token = get_token()
+    if token:
+        sp = spotipy.Spotify(auth=token)
+    else:
+        logging.info("Can't get token for", username)
+        return
+
+    for bucket in bucket_uris:
+        avg_bucket_relevant_features = {}
+        danceability = []
+        energy = []
+        instrumentalness = []
+        liveness = []
+        loudness = []
+        speechiness = []
+        valence = []
+
+        features = sp.audio_features(bucket)
+
+        for feature in features:
+            danceability.append(feature['danceability']) 
+            energy.append(feature['energy'])
+            instrumentalness.append(feature['instrumentalness'])
+            liveness.append(feature['liveness'])
+            loudness.append(feature['loudness'])
+            speechiness.append(feature['speechiness'])
+            valence.append(feature['valence'])
+
+        avg_bucket_relevant_features['danceability'] = np.average(danceability)
+        avg_bucket_relevant_features['energy'] = np.average(energy)
+        avg_bucket_relevant_features['instrumentalness'] = np.average(instrumentalness)
+        avg_bucket_relevant_features['liveness'] = np.average(liveness)
+        avg_bucket_relevant_features['loudness'] = np.average(loudness)
+        avg_bucket_relevant_features['speechiness'] = np.average(speechiness)
+        avg_bucket_relevant_features['valence'] = np.average(valence)
+        
+        final_buckets.append(avg_bucket_relevant_features)
+        
+    return final_buckets
 
 def get_uris():
     uris = []
@@ -28,20 +83,15 @@ def get_uris():
 def main(req: func.HttpRequest) -> func.HttpResponse:
     uris = []
     logging.info('Python HTTP trigger function processed a request.')
-    if (req.params.get('req_type') == 'get_uris'):
+    
+    if (req.params.get('analysis') == 'features'):
+        bucket_uris = get_uris()
         return func.HttpResponse(
             status_code = 200,
-            body = json.dumps(get_uris())
-        )
-    elif (req.params.get('req_type') == 'song_analysis'):
-        return func.HttpResponse(
-            body = json.dumps(song_analysis())
+            body = json.dumps(feature_analysis(bucket_uris))
         )
     else:
         return func.HttpResponse(
             body = "Check req_type parameter",
             status_code = 404
-        ) 
-    return func.HttpResponse(
-        status_code = 200
-    )
+        )
